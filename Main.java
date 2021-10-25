f2d5a1f19a14e533837271c33537b3d960700589
isaac <59864350+sindiga1@users.noreply.github.com>
2021-10-25 09:54:34 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,3 +5,11 @@ public class Main{
     System.out.println(Math.max(x,y));
   }  
 }
+//The Math.min(x,y) method can be used to find the lowest value of x and y
+public class Main{
+  public static void main(String[]args){
+    int x=8;
+    int y=8;
+    System.out.println(Math.min(x,y));
+  }
+}  
